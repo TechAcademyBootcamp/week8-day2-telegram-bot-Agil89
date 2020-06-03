814ffa827c4ed2f257b277fdce36e269f2ccb5e4
muradddd <murad.rustemzade@gmail.com>
2020-06-03 18:41:32 +0400

    minor changes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from time import sleep
 from talk import *
 
 update_id = None
+learning = False
 def main():
     global update_id
     bot = telegram.Bot(token="1211173538:AAFM0ki6145h4l0bUC654T5t-_n7cBVdPaM")
@@ -20,6 +21,7 @@ def main():
 
 def echo(bot):
     global update_id
+    global learning
     for update in bot.get_updates(offset=update_id, timeout=10):
         update_id = update.update_id + 1
         l = ['exchange','news','weather','search']
@@ -27,6 +29,7 @@ def echo(bot):
             query = update.callback_query
             if query.answer():
                 if query.data == "YES":
+                    learning = True
                     query.edit_message_text(
                         text="Suala cavab verin: ",
                         reply_markup=None
@@ -34,33 +37,27 @@ def echo(bot):
         if update.message:
             message = update.message.text
             if message not in l or not message.startswith('search'):
-                checkFile(message)
-                if checkFile(message)==None:
+                answer = checkFile(message, learning)
+                print(answer)
+                if answer:
+                    bot.send_message(chat_id=update.message.chat_id, text=f'{answer}')
+                elif not learning:
+                    def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
+                        menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+                        if header_buttons:
+                            menu.insert(0, header_buttons)
+                        if footer_buttons:
+                            menu.append(footer_buttons)
+                        return menu
+                    # update.message.reply_text('Suali mene oyretmek isteyirsiniz mi? ')
+                    list_of_questions = ['YES','NO']
+                    button_list = []
+                    for each in list_of_questions:
+                        button_list.append(telegram.InlineKeyboardButton(each, callback_data=each))
+                    reply_markup = telegram.InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
+                    bot.send_message(chat_id=update.message.chat_id, text='Suali mene oyretmek isteyirsiniz mi?',
+                                        reply_markup=reply_markup)
 
-                    # question = message
-                    # update.message.reply_text(question)
-                    if message not in data_dict and data:
-                        question = message
-                        f.write(f'{question}::{None}\n')
-
-                        def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
-                            menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-                            if header_buttons:
-                                menu.insert(0, header_buttons)
-                            if footer_buttons:
-                                menu.append(footer_buttons)
-                            return menu
-                        # update.message.reply_text('Suali mene oyretmek isteyirsiniz mi? ')
-                        list_of_questions = ['YES','NO']
-                        button_list = []
-                        for each in list_of_questions:
-                            button_list.append(telegram.InlineKeyboardButton(each, callback_data=each))
-                        reply_markup = telegram.InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
-                        bot.send_message(chat_id=update.message.chat_id, text='Suali mene oyretmek isteyirsiniz mi?',
-                                         reply_markup=reply_markup)
-
-            if message =='salam':
-                update.message.reply_text('Aleykum salam.Xeberler ucun news,hava melumati ucun weather,mezenneni oyrenmek uchun exchange,goole-da search ucun search+axtarilacaq soz,sual cavab ucun ise start frazalarindan istifade ede bilersiniz')
             if message =='exchange':
                 update.message.reply_text(exchange())
             if message =='news':
@@ -70,7 +67,10 @@ def echo(bot):
             if 'search' in message:
                 word = message.split(' ')[1]
                 update.message.reply_text(googleRequest(word))
-
+            
+            if message and learning:
+                print('oyretdi', message)
+                learning = False
 
 if __name__ == '__main__':
     main()
--- talk.py
+++ talk.py
@@ -1,29 +1,20 @@
-def checkFile():
+def checkFile(word, learning):
     with open('bot.txt','a+') as f:
         f.seek(0)
         for x in f.readlines():
             if word == x.split('::')[0]:
                 return readFromFile(word)
-            else:
-                def build_menu(buttons, n_cols, header_buttons=None, footer_buttons=None):
-                    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-                    if header_buttons:
-                        menu.insert(0, header_buttons)
-                    if footer_buttons:
-                        menu.append(footer_buttons)
-                    return menu
-                list_of_questions = ['YES', 'NO']
-                button_list = []
-                for each in list_of_questions:
-                    button_list.append(telegram.InlineKeyboardButton(each, callback_data=each))
-                reply_markup = telegram.InlineKeyboardMarkup(build_menu(button_list, n_cols=1))
-                bot.send_message(chat_id=update.message.chat_id, text='Suali mene oyretmek isteyirsiniz mi?',
-                                 reply_markup=reply_markup)
+            print('yazdi')
+            return addToFile(word, learning)
 
-def addToFile(key,val):
+def addToFile(word, learning):
+    print('add to file')
     with open('bot.txt', 'a+') as f:
-        f.seek(0)
-        f.write(f'{key}::{val}')
+        # f.seek(0)
+        if not learning:
+            f.write(f'{word}::')
+        else:
+            f.write(f'{word}\n')
 
 def readFromFile(word):
     with open('bot.txt', 'a+') as f:
@@ -31,7 +22,3 @@ def readFromFile(word):
         for x in f.readlines():
             if x.split('::')[0] == word:
                 return x.split('::')[1]
-
-
-
-checkFile()
